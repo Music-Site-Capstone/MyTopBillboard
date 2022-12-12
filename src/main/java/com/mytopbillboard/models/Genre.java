8b8395272b35a72d7d1cfaa54bc2890c29a89034
Daniel Name <daniel.v.name@gmail.com>
2022-12-12 12:31:49 -0600

    refactored

--- src/main/java/com/mytopbillboard/models/Genre.java
+++ src/main/java/com/mytopbillboard/models/Genre.java
@@ -1,32 +1,28 @@
 package com.mytopbillboard.models;
 
 public class Genre {
+    public Genre(long id, String name) {
+        this.name = name;
+        this.id = id;
+    }
 
-    private String name;
+    public Genre() {}
 
     private long id;
+        public long getId() {
+            return id;
+        }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
+        public void setId(long id) {
         this.id = id;
     }
 
-    public Genre() {
-    }
+    private String name;
+        public String getName() {
+            return name;
+        }
 
-    public Genre(String name, long id) {
+        public void setName(String name) {
         this.name = name;
-        this.id = id;
     }
 }
--- src/main/java/com/mytopbillboard/models/Song.java
+++ src/main/java/com/mytopbillboard/models/Song.java
@@ -2,24 +2,11 @@ package com.mytopbillboard.models;
 
 public class Song {
 
-    private String title;
-
-    private long artist_id;
-
-    private long genre_id;
-
-    private long song_id;
-
-    public Song() {
-    }
-
-    public Song(String title) {
-        this.title = title;
-    }
-
-    public Song(String title, long artist_id) {
+    public Song(long song_id, String title, long artist_id, long genre_id) {
         this.title = title;
         this.artist_id = artist_id;
+        this.genre_id = genre_id;
+        this.song_id = song_id;
     }
 
     public Song(String title, long artist_id, long genre_id) {
@@ -28,13 +15,27 @@ public class Song {
         this.genre_id = genre_id;
     }
 
-    public Song(String title, long artist_id, long genre_id, long song_id) {
+    public Song(String title, long artist_id) {
         this.title = title;
         this.artist_id = artist_id;
-        this.genre_id = genre_id;
-        this.song_id = song_id;
     }
 
+    public Song(String title) {
+        this.title = title;
+    }
+
+    public Song() {}
+
+    private long song_id;
+        public long getSong_id() {
+            return song_id;
+        }
+
+        public void setSong_id(long song_id) {
+            this.song_id = song_id;
+        }
+
+    private String title;
     public String getTitle() {
         return title;
     }
@@ -43,28 +44,21 @@ public class Song {
         this.title = title;
     }
 
-    public long getArtist_id() {
-        return artist_id;
-    }
+    private long artist_id;
+        public long getArtist_id() {
+            return artist_id;
+        }
 
-    public void setArtist_id(long artist_id) {
-        this.artist_id = artist_id;
-    }
+        public void setArtist_id(long artist_id) {
+            this.artist_id = artist_id;
+        }
 
-    public long getGenre_id() {
-        return genre_id;
-    }
+    private long genre_id;
+        public long getGenre_id() {
+            return genre_id;
+        }
 
-    public void setGenre_id(long genre_id) {
-        this.genre_id = genre_id;
-    }
-
-    public long getSong_id() {
-        return song_id;
-    }
-
-    public void setSong_id(long song_id) {
-        this.song_id = song_id;
-    }
-
+        public void setGenre_id(long genre_id) {
+            this.genre_id = genre_id;
+        }
 }
