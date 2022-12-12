a3b709b764c009557c6eb687729fb3fdcaf8947f
Daniel Name <daniel.v.name@gmail.com>
2022-12-12 14:17:01 -0600

    got program running, added user controller

--- src/main/java/com/mytopbillboard/repositories/GenreRepository.java
+++ src/main/java/com/mytopbillboard/repositories/GenreRepository.java
@@ -1,10 +1,10 @@
-package com.mytopbillboard.repositories;
-
-import com.mytopbillboard.models.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-
-    Genre findById(long id);
-
-}
+//package com.mytopbillboard.repositories;
+//
+//import com.mytopbillboard.models.Genre;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface GenreRepository extends JpaRepository<Genre, Long> {
+//
+//    Genre findById(long id);
+//
+//}
