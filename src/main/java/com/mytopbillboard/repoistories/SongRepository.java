4fb862562336746bfed51460e1ec3820652c9c24
Daniel Name <daniel.v.name@gmail.com>
2022-12-12 15:49:52 -0600

    recreated repoistories

---
+++ src/main/java/com/mytopbillboard/repoistories/SongRepository.java
@@ -0,0 +1,12 @@
+//package com.mytopbillboard.repositories;
+//
+//import com.mytopbillboard.models.Song;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface SongRepository extends JpaRepository<Song, Long> {
+//
+//    Song findById(long id);
+//
+//    Song findByTitle(String title);
+//
+//}
