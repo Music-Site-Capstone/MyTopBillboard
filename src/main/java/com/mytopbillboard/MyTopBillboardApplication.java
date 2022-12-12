bf107431ecd11c0c599ad522bbc5e9529b399c7d
Richard Kim <richardjkim7@gmail.com>
2022-12-12 10:38:05 -0600

    first push

---
+++ src/main/java/com/mytopbillboard/MyTopBillboardApplication.java
@@ -0,0 +1,13 @@
+package com.mytopbillboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTopBillboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTopBillboardApplication.class, args);
+    }
+
+}
