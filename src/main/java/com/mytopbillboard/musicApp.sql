e00a8e708c557f477f23c59256c6e62bc7b3e676
Daniel Name <daniel.v.name@gmail.com>
2022-12-13 09:42:33 -0600

    created databases

---
+++ src/main/java/com/mytopbillboard/musicApp.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS mtb_db;
+CREATE DATABASE IF NOT EXISTS mtb_db;
+GRANT ALL ON mtb_db.* TO 'codeup_test_user'@'localhost';
+USE mtb_db;
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (id INT NOT NULL AUTO_INCREMENT,
+                     username VARCHAR(50) NOT NULL,
+                     email VARCHAR(100) NOT NULL,
+                     password VARCHAR(50),
+                     PRIMARY KEY (id),
+                     UNIQUE (username, email)
+);
+DROP TABLE IF EXISTS playlist;
+CREATE TABLE playlist (id INT NOT NULL AUTO_INCREMENT,
+                   user_id INT NOT NULL,
+                   profile_name VARCHAR(50),
+                   PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS rating;
+CREATE TABLE rating (id INT NOT NULL AUTO_INCREMENT,
+                       user_id INT NOT NULL,
+                       playlist_id INT NOT NULL,
+                       score INT,
+                       PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS song;
+CREATE TABLE song (id INT NOT NULL AUTO_INCREMENT,
+                     artist_id INT NOT NULL,
+                     genre_id INT NOT NULL,
+                     title VARCHAR(255),
+                     PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS artist;
+CREATE TABLE artist (id INT NOT NULL AUTO_INCREMENT,
+                   name VARCHAR(75),
+                   PRIMARY KEY (id)
+);
+DROP TABLE IF EXISTS genre;
+CREATE TABLE genre (id INT NOT NULL AUTO_INCREMENT,
+                     name VARCHAR(75),
+                     PRIMARY KEY (id)
+);
