d0620be8d9c2af675c542e7496fa2670a21219b8
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-12-14 13:17:07 -0600

    Altered User Table, Added Password Encoder, Added GetMapping and PostMapping in UserController

--- src/main/java/com/mytopbillboard/controllers/UserController.java
+++ src/main/java/com/mytopbillboard/controllers/UserController.java
@@ -1,13 +1,45 @@
 package com.mytopbillboard.controllers;
 
+import com.mytopbillboard.models.User;
+import com.mytopbillboard.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    private final UserRepository userDao;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder){
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @GetMapping("/profile")
     public String profile(){
         return "siteViews/profile";
     }
+
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        model.addAttribute("user", new User());
+        return "/registration";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userDao.save(user);
+        return "redirect:/homepage";
+    }
+
 }
