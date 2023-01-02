df1bad83e6834aab23ea8dcdf4eedafb32767652
Daniel Name <daniel.v.name@gmail.com>
2023-01-02 14:33:17 -0600

    attached playlist submit to enter button

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -4,4 +4,10 @@ $(function (){
         $('.playlist-title').text($(this).text());
         $('.song-list').html('').append();//??
     })
+
+    $('#newPlaylistInput').on('keypress',function(e) {
+        if(e.which === 13 || e.keyCode === 13) {
+            $('.playlistForm').submit();
+        }
+    });
 })
