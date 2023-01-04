c8437f8b1e838de02d8f20dcf538663d5520933c
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2023-01-04 09:49:50 -0600

    updated spotify_api.js

--- src/main/resources/static/js/spotify_api.js
+++ src/main/resources/static/js/spotify_api.js
@@ -100,7 +100,6 @@ SpotifyAPIController = (async function() {
 
             $('.modal-fill').append(`<div class="searchline"><img src="${image}" alt="fail"><p>${artist} - ${track}</p>
             <form th:action="@{profile/pageOwner}" th:method="POST" th:object="${artist}" th:object="${genres}">
-            <input th:field="*{}"
             <button class="addButton" id="addGenres">Add song to playlist</button>
             </form>
             </div>`);
