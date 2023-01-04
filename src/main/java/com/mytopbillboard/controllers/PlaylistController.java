8fe3205e1f0ed8fa84461d914778d2fc81509d5b
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2023-01-04 15:39:18 -0600

    merged with main and made tiny tweaks

--- src/main/java/com/mytopbillboard/controllers/PlaylistController.java
+++ src/main/java/com/mytopbillboard/controllers/PlaylistController.java
@@ -49,7 +49,7 @@ public class PlaylistController {
     }
 
     @PostMapping("/song/playlist/{playlistId}/add/{songId}")
-    public String addSongToPlaylist(@PathVariable long playlistId, @PathVariable long songId, @RequestBody ){
+    public String addSongToPlaylist(@PathVariable long playlistId, @PathVariable long songId){
         Playlist playlist = playlistDao.findById(playlistId);
         List<Song> songs = playlist.getSong();
         //Get artist information<Name>
