17b926021f5efd28d7ff9702ad22dc81dc6da393
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2023-01-17 15:08:55 -0600

    Added trophies back to the leaderboard

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -6,3 +6,8 @@ $(document).ready(function () {
         window.location.replace('/profile/' + formValue);
     });
 })
+
+$(".rank-number:contains('1')").parent().append('<i class="bi goldTrophy bi-trophy-fill"></i>');
+$(".rank-number:contains('2')").parent().append('<i class="bi silverTrophy bi-trophy-fill"></i>');
+$(".rank-number:contains('3')").parent().append('<i class="bi bronzeTrophy bi-trophy-fill"></i>');
+$(".rank-number:contains('10')").parent().find('.bi-trophy-fill').remove();
